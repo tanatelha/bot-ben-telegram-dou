2636fe0a91b32416826e9659cdc244e1a6223a7b
Natália Santos <89846568+tanatelha@users.noreply.github.com>
2023-04-03 19:06:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -144,52 +144,49 @@ def telegram_bot():
 
 
 def telegram_bot_envio():
-  enviadas = []
-  resposta = ""
   data_atual = data_hoje()
 
-  apresentacao = f'<b>Bom dia, humana!</b> \U0001F31E	\n \nVamos lá para os destaques do <i>Diário Oficial da União</i> de hoje! \n \n<b>{data_atual}</b> \n'
+  apresentacao = f'<b>Bom dia, humana!</b> \U0001F31E	\n \nVamos lá para os destaques do <i>Diário Oficial da União</i> de hoje! \n \n<b>{data_hoje()}</b> \n'
   finalizacao = f'Para mais informações, <a href="https://www.in.gov.br/servicos/diario-oficial-da-uniao">acesse o site do DOU</a>'
-  
+
   resposta = requests.get('https://www.in.gov.br/servicos/diario-oficial-da-uniao/destaques-do-diario-oficial-da-uniao', params=None)
   site = BeautifulSoup(resposta.content, features="html.parser")
   lista_materias = site.findAll('div', {'class' : 'dou row'}) #parte do site html que tem as matérias
- 
-  mensagem_destaque_lista = []
-  
+
+  texto = apresentacao
+
+  lista = []
+
   for materia in lista_materias:
     noticia = materia
     data = (noticia.find('p', {'class' : 'date'})).text
 
-
-    if data == data_atual:
+    if data == data_hoje():
+      data = (noticia.find('p', {'class' : 'date'})).text
       pasta = noticia.find('p').text
       manchete = noticia.find('a').text
       link = noticia.find('a').get('href')
 
       manchete_item = f"\N{card index dividers} <b>{pasta}</b> \n{manchete} | <a href='{link}'>Acesse aqui a decisão</a> "
-      mensagem_destaque_lista.append(manchete_item)
-      
-      # montagem texto
-      texto_final = apresentacao
-      for i in mensagem_destaque_lista:
-        texto_final += f'\n \n{i}'
+      lista.append(manchete_item)
 
-      texto_resposta = f'{texto_final} \n \n {finalizacao}'
+  if lista:
+    for item in lista:
+      texto += f'{item}'
+      texto_resposta = texto
+
+  if not lista:
+    texto_resposta = f'<b>Bom dia, humana!</b> \U0001F31E \n \nNão tem Destaques do DOU para o dia de hoje! \n \n<i>Pode descansar e fazer outra coisa! \U0001F973</i>'
     
-    else:
-      texto_resposta = f'<b>Bom dia, humana!</b> \U0001F31E \n \nNão tem Destaques do DOU para o dia de hoje! \n \n<i>Pode descansar e fazer outra coisa! \U0001F973</i>'
-  
+  mensagem = {"chat_id": TELEGRAM_ADMIN_ID, "text": texto_resposta, "parse_mode": 'html'}
+  requests.post(f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage", data=mensagem)
     
-    mensagem = {"chat_id": TELEGRAM_ADMIN_ID, "text": texto_resposta, "parse_mode": 'html'}
-    requests.post(f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage", data=mensagem)
-    
-    enviadas.append([data_atual, str(time), "enviada", TELEGRAM_ADMIN_ID, texto_resposta])
+  enviadas.append([data_atual, str(time), "enviada", TELEGRAM_ADMIN_ID, texto_resposta])
 
   ### Atualizando a planilha sheets ss mensagens enviadas
-  sheet_enviadas.append_rows(enviadas)
+sheet_enviadas.append_rows(enviadas)
   
-  return "ok"
+return "ok"
   
   
   
