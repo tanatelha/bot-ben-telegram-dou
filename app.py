5731d135864437104aa24247f7f9a682344f69b2
Natália Santos <89846568+tanatelha@users.noreply.github.com>
2023-04-05 16:10:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -196,8 +196,12 @@ def telegram_bot_envio():
 
     inscritos = sheet_inscritos.col_values(6)
     inscritos = list(set(inscritos))
-    inscritos.remove('')
-    lista_inicial.append(inscritos)
+    if '' in inscritos:
+        inscritos.remove('')
+        lista_inicial.append(inscritos)
+    else:
+        lista_inicial.append(inscritos)
+        
 
     for item in lista_inicial:
         inscritos_final.extend(item)
