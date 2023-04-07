ee221107fbaa12a0554ba8d24a27e4609b3a22b7
Natália Santos <89846568+tanatelha@users.noreply.github.com>
2023-04-07 16:00:16 -0300

    Create inscritos.py

---
+++ inscritos.py
@@ -0,0 +1,17 @@
+def identificar_inscritos():
+    lista_inicial = []
+    inscritos_final = []
+
+    inscritos = sheet_inscritos.col_values(6)
+    inscritos = list(set(inscritos))
+    if '' in inscritos:
+        inscritos.remove('')
+        lista_inicial.append(inscritos)
+    else:
+        lista_inicial.append(inscritos)
+
+    # fazendo apenas uma lista
+    for sublista in lista_inicial:
+        inscritos_final.extend(sublista)
+
+    return inscritos_final
