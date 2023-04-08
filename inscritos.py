5dcc165f75aeb90c04e831a4c87c5800f55f05e9
Natália Santos <89846568+tanatelha@users.noreply.github.com>
2023-04-08 11:03:27 -0300

    Update inscritos.py

--- inscritos.py
+++ inscritos.py
@@ -1,3 +1,21 @@
+import os
+import gspread
+
+GOOGLE_SHEETS_KEY = os.environ["GOOGLE_SHEETS_KEY"] 
+
+GOOGLE_SHEETS_CREDENTIALS = os.environ['GOOGLE_SHEETS_CREDENTIALS']
+with open("credenciais.json", mode="w") as arquivo:
+    arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
+conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
+
+api = gspread.authorize(conta)
+planilha = api.open_by_key(f'{GOOGLE_SHEETS_KEY}') 
+sheet_inscritos = planilha.worksheet('inscritos')
+sheet_descadastrados = planilha.worksheet('descadastrados')
+
+
+
+
 def identificar_inscritos():
     lista_inicial = []
     inscritos_final = []
