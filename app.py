7d60a44ccf236eba3fb5b403564f57f6adfca147
Natália Santos <89846568+tanatelha@users.noreply.github.com>
2023-04-08 11:15:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -100,13 +100,13 @@ def telegram_bot():
             texto_resposta = f'voce já está inscrito meu irmao'
             nova_mensagem = {"chat_id": chat_id, "text": texto_resposta, "parse_mode": 'html'}
             resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_TOKEN}/sendMessage", data = nova_mensagem)
-            mensagens.append([str(date), str(time), "recebida", username, first_name, chat_id, texto])
+            mensagens.append([str(date), str(time), "recebida", username, first_name, chat_id, texto_resposta])
            
         else:
             texto_resposta = "Olá, humana! \n \nEu sou o <b>Benjamin do Diário Oficial da União</b>, mas você pode me chamar de <b>Ben do DOU</b>!  Ou apenas Ben... \U0001F916 \n \nSou um bot criado para enviar diariamente, por meio do Telegram, os destaques do Executivo publicados no <i>Diário Oficial da União</i>. \n \nVocê acaba de se inscrever para receber os destaques do DOU! As mensagens serão enviadas todos os dias a partir das 7h da manhã. \n \nSeja bem-vinda! \U0001F609"
             nova_mensagem = {"chat_id": chat_id, "text": texto_resposta, "parse_mode": 'html'}
             resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_TOKEN}/sendMessage", data = nova_mensagem)
-            inscricoes.append([str(date), str(time), "inscrito", first_name, last_name, username, sender_id, chat_id, texto])
+            inscricoes.append([str(date), str(time), "inscrito", first_name, last_name, username, sender_id, chat_id, texto_resposta])
             
 
   elif message == "/descadrastar":
